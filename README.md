472e34cc854ac0fade826f0248b2c9030db86f42
Ali Rostami <rostami@axxessio.com>
2023-05-07 09:00:34 +0200

    mvn exec added

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 The Java program should support the following command line arguments:
 ```
-java org.yourname.Main \
+java org.ali.Main \
 --input <ascii-file-containing-strings-separated-by-linebreak.txt> \
 --inputtype <string|int|double> \
 --operations <operation1[,operation2[...]]> \
 --threads <n> \
 [--output <output.txt>]
 ```
+
+The program can be build and executed using maven as follows:
+```
+mvn clean package
+mvn exec:java -Dexec.args="--input example2_strings.txt --inputtype string --operations rev --threads 4 --output output.txt"
+```
+
--- src/org/ali/FileProcessingTask.java
+++ src/org/ali/FileProcessingTask.java
@@ -13,14 +13,27 @@ import java.io.FileReader;
  */
 public class FileProcessingTask implements Runnable {
 
+    /**
+     * inputFileName: the name of the input file
+     * startLine: the line number to start processing
+     * endLine: the line number to end processing
+     * operations: the operations to be applied on the file
+     * writer: the writer to write the output to
+     */
     private final String inputFileName;
     private final long startLine;
     private final long endLine;
-
     private final Operations operations;
-
     private final BufferedWriter writer;
 
+    /**
+     * Constructor
+     * @param inputFileName the name of the input file
+     * @param startLine the line number to start processing
+     * @param endLine the line number to end processing
+     * @param operations the operations to be applied on the file
+     * @param writer the writer to write the output to
+     */
     public FileProcessingTask(String inputFileName, long startLine, long endLine, Operations operations,
                               BufferedWriter writer) {
         this.inputFileName = inputFileName;
@@ -30,20 +43,28 @@ public class FileProcessingTask implements Runnable {
         this.writer = writer;
     }
 
+    /**
+     * Run method
+     * This method is called when the thread is started.
+     * It reads the input file line by line and applies the operations on each line.
+     * It writes the output to the writer.
+     */
     @Override
     public void run() {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
-
             String line;
             long lineNumber = 0;
 
             while ((line = reader.readLine()) != null) {
                 if (lineNumber >= startLine && lineNumber <= endLine) {
+
                     line = operations.applyForMultiThreading(line);
                     synchronized(writer) {
-                        if(line.length() >= 1)
-                            writer.write(line+"\n");
+                        if(line.length() >= 1) {
+                            Statistics.getInstance().updateStatisticsWithLine(line);
+                            writer.write(line + "\n");
+                        }
                     }
                 } else if (lineNumber > endLine) {
                     break;
